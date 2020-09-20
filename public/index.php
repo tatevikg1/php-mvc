e89697458b97495539bc057f8426d9ba63aca3eb
Ta Gr <tatevikg1@gmail.com>
2020-09-21 00:13:38 +0400

    request method function, auth controller, send parametrs to views, get data from the users

--- public/index.php
+++ public/index.php
@@ -4,19 +4,26 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use app\controllers\FormController;
+use app\controllers\AuthController;
 use app\core\Application;
-
 
 
 $app = new Application();
 
+
 $app->router->get('/', 'content');
 
-$app->router->get('/layouts', 'layouts');
-
 $app->router->get('/form', [FormController::class, 'index']);
-
 $app->router->post('/post', [FormController::class, 'store']);
 
+// auth routes
+$app->router->get   ('/register',   [AuthController::class, 'register']);
+$app->router->post  ('/register',   [AuthController::class, 'register']);
+$app->router->get   ('/login',      [AuthController::class, 'login']);
+$app->router->post  ('/login',      [AuthController::class, 'login']);
 
 $app->run();
+
+// MESSAGE TO MYSELF
+
+// Run the app from public folder where the index.php is(the program starts with this file)
